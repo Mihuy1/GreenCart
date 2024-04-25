2d0fb95c9ec951dec898767f199808f545711fda
nimofarah <faranim01@gmail.com>
2024-04-25 23:44:21 +0200

    Update shoppingcart

--- js/shoppingCart.js
+++ js/shoppingCart.js
@@ -1,4 +1,4 @@
-const smileyModal = document.querySelector('.smiley-modal');
+/*const smileyModal = document.querySelector('.smiley-modal');
 const completeButton = document.querySelector('.complete-button');
 
 const INPUTS = document.querySelectorAll('#smileys input');
@@ -10,4 +10,39 @@ INPUTS.forEach((el) => el.addEventListener('click', (e) => updateValue(e)));
 completeButton.addEventListener('click', function (evt) {
   evt.preventDefault();
   smileyModal.close();
-});
+});*/
+
+let cartIcon = document.querySelector("#cart-icon")
+let cart = document.querySelector(".shopping-cart")
+let closeCart = document.querySelector("#close-cart")
+
+
+//Opening Cart
+cartIcon.onclick = () => {
+  cart.classList.add("active");
+};
+
+
+//Closing Cart
+closeCart.onclick = () => {
+  cart.classList.remove("active");
+};
+
+//Cart Working
+if (document.readyState == "loading") {
+  document.addEventListener("DOMContentLoaded", ready);
+} else {
+  ready();
+}
+
+//Making Function
+
+function ready () {
+  //Remove item from cart
+  var removeCartButton = document.getElementsByClassName("cart-removing")
+  console.log(removeCartButton)
+  for (var i = 0; i < removeCartButton.length; i++){
+    var button = removeCartButton [i]
+    button.addEventListener('click' removeCartItem)
+  }
+}
